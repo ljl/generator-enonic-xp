45ce396206edd8f17a61415f14057096aec30cf8
Lasse Jul-Larsen <lassejl@gmail.com>
2017-01-09 19:00:21 +0100

    Add child content switch

--- generators/contenttype/index.js
+++ generators/contenttype/index.js
@@ -20,6 +20,7 @@ module.exports = yeoman.Base.extend({
       this.destinationPath('src/main/resources/site/content-types/' + this.props.name + '/' + this.props.name + '.xml'), {
         name: this.props.name,
         supertype: this.props.supertype,
+        allowChildren: this.props.allowChildren,
         fields: inputFields
       }
     );
@@ -37,6 +38,11 @@ function askForData() {
     name: 'supertype',
     message: 'Supertype?',
     default: 'base:structured'
+  }, {
+    type: 'confirm',
+    name: 'allowChildren',
+    message: 'Allow child content?',
+    default: true
   }];
 
   return this.prompt(prompts).then(function (props) {
